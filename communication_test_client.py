0f6e0a04cf9825dae73729766545e25f9998967f
brianpage <bpage@mtu.edu>
2017-12-07 10:09:00 -0500

    as

--- communication_test_client.py
+++ communication_test_client.py
@@ -3,6 +3,6 @@ from communication import MessageVerification, getIP
 com = MessageVerification(True) # "True" for verbose
 # my_ip = getIP() # get IP of this machine
 com.connectToServer("192.168.1.26") # should be changed to the address of other node communicating between two machines
-com.sendMessage("$BFLOG,ACK,ON".encode()))# send's a "something" message
+com.sendMessage("$BFLOG,ACK,ON".encode())# send's a "something" message
 com.verifyMessage("what now") # verify if a "what now" message has been received
 com.close()
