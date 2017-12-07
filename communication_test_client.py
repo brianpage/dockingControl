c120c71f85d2e1d90948e637f2e043821e3c54c7
brianpage <bpage@mtu.edu>
2017-12-07 10:28:33 -0500

    encode

--- communication_test_client.py
+++ communication_test_client.py
@@ -4,7 +4,7 @@ com = MessageVerification(True) # "True" for verbose
 # my_ip = getIP() # get IP of this machine
 com.connectToServer("192.168.1.26") # should be changed to the address of other node communicating between two machines
 com.verifyMessage("what now") # verify if a "what now" message has been received
-com.sendMessage("$BPLOG,ACK,ON".encode())# send's a "something" message
+com.sendMessage("$BPLOG,ACK,ON")# send's a "something" message
 com.verifyMessage("what now") # verify if a "what now" message has been received
 com.verifyMessage("what now") # verify if a "what now" message has been received
 com.close()
