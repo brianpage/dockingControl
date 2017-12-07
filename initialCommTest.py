f3121253f5bb5000482fb76ff6a18575f2afe033
brianpage <bpage@mtu.edu>
2017-12-07 15:10:36 -0500

    a

--- initialCommTest.py
+++ initialCommTest.py
@@ -13,7 +13,7 @@ s.connect((TCP_IP, TCP_PORT))
 
 data = s.recv(BUFFER_SIZE)
 print("startup",data.decode())
-s.send(MESSAGE.encode())
+s.send(MESSAGE)#.encode())
 s.shutdown(socket.SHUT_WR)
 
 count=0
